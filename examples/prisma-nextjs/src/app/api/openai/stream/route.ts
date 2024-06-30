e5efec96da7ea43969bbd23aed16a0c7feccc830
Domas <domas.bitvinskas@me.com>
2024-06-30 19:49:33 +0100

    Add openaiClientAdapter; more tests

--- examples/prisma-nextjs/src/app/api/openai/stream/route.ts
+++ examples/prisma-nextjs/src/app/api/openai/stream/route.ts
@@ -1,11 +1,11 @@
+import OpenAI from 'openai'
 import { NextResponse } from 'next/server'
 import {
   supercompat,
-  groqClientAdapter,
+  openaiClientAdapter,
   prismaStorageAdapter,
   completionsRunAdapter,
 } from 'supercompat'
-import Groq from 'groq-sdk'
 import { prisma } from '@/lib/prisma'
 
 const tools = [
@@ -27,18 +27,13 @@ const tools = [
       },
     }
   }
-];
+] as OpenAI.Beta.AssistantTool[]
 
 export const GET = async () => {
   const client = supercompat({
-    client: groqClientAdapter({
-      groq: new Groq(),
+    client: openaiClientAdapter({
+      openai: new OpenAI(),
     }),
-    // client: groqClientAdapter({
-    //   groq: new OpenAI({
-    //     apiKey: process.env.OPENAI_API_KEY!,
-    //   }),
-    // }),
     storage: prismaStorageAdapter({
       prisma,
     }),
@@ -59,38 +54,36 @@ export const GET = async () => {
     content: 'What is the weather in SF?'
   })
 
-  const ru = await client.beta.threads.runs.create(
+  const run = await client.beta.threads.runs.create(
     thread.id,
     {
       assistant_id: assistantId,
       instructions: 'Use the get_current_weather and then answer the message.',
-      model: 'llama3-8b-8192',
+      model: 'gpt-3.5-turbo',
       stream: true,
       tools,
       truncation_strategy: {
         type: 'last_messages',
         last_messages: 10,
       },
-      // model: 'gpt-3.5-turbo',
     },
   )
 
   let requiresActionEvent
 
-  for await (const event of ru) {
+  for await (const event of run) {
     if (event.event === 'thread.run.requires_action') {
       requiresActionEvent = event
     }
   }
 
-  console.dir({ requiresActionEvent }, { depth: null })
   if (!requiresActionEvent) {
     throw new Error('No requires action event')
   }
 
   const toolCallId = requiresActionEvent.data.required_action?.submit_tool_outputs.tool_calls[0].id
 
-  const run = await client.beta.threads.runs.submitToolOutputs(
+  const submitToolOutputsRun = await client.beta.threads.runs.submitToolOutputs(
     thread.id,
     requiresActionEvent.data.id,
     {
@@ -98,14 +91,13 @@ export const GET = async () => {
       tool_outputs: [
         {
           tool_call_id: toolCallId,
-          output: "70 degrees and sunny.",
+          output: '70 degrees and sunny.',
         },
       ],
     }
   )
 
-  for await (const event of run) {
-    // console.dir({ event }, { depth: null })
+  for await (const _event of submitToolOutputsRun) {
   }
 
   const threadMessages = await client.beta.threads.messages.list(thread.id, { limit: 10 })
@@ -113,27 +105,4 @@ export const GET = async () => {
   return NextResponse.json({
     threadMessages,
   })
-
-
-  // await client.beta.threads.runs.createAndPoll(
-  //   thread.id,
-  //   {
-  //     assistant_id: assistantId,
-  //     instructions: 'Just reply',
-  //     model: 'llama3-8b-8192',
-  //     // model: 'gpt-3.5-turbo',
-  //   },
-  // )
-  //
-  // const threadMessages = await client.beta.threads.messages.list(thread.id, { limit: 10 })
-  //
-  // const runs = await client.beta.threads.runs.list(thread.id)
-  // const last = runs.data[runs.data.length - 1]
-  // console.dir({last}, { depth: null })
-  // const steps = await client.beta.threads.runs.steps.list(thread.id, last.id)
-  // console.dir({steps}, { depth: null })
-  //
-  // return NextResponse.json({
-  //   threadMessages,
-  // })
 }
---
+++ packages/supercompat/src/adapters/client/openaiClientAdapter/completions/post.ts
@@ -0,0 +1,54 @@
+import type OpenAI from 'openai'
+
+export const post = ({
+  openai,
+}: {
+  openai: OpenAI
+}) => async (_url: string, options: any) => {
+  const body = JSON.parse(options.body)
+
+  if (body.stream) {
+    const response = await openai.chat.completions.create(body)
+
+    const stream = new ReadableStream({
+      async start(controller) {
+        // @ts-ignore-next-line
+        for await (const chunk of response) {
+          controller.enqueue(`data: ${JSON.stringify(chunk)}\n\n`)
+        }
+
+        controller.close()
+      },
+    })
+
+    return new Response(stream, {
+      headers: {
+        'Content-Type': 'text/event-stream',
+      },
+    })
+  } else {
+    try {
+      const data = await openai.chat.completions.create(body)
+
+      return new Response(JSON.stringify({
+        data,
+      }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    } catch (error) {
+      console.dir({ error }, { depth: null })
+      // @ts-ignore-next-line
+      return new Response(JSON.stringify({
+        error,
+      }), {
+        status: 500,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+    }
+  }
+}
