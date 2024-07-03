5e368556ff23e59cfdef99daa068da467cd5ef8b
Domas <domas.bitvinskas@me.com>
2024-07-03 03:16:53 +0100

    Handle no tools on Anthropic

--- packages/supercompat/src/adapters/client/anthropicClientAdapter/completions/serializeTools.ts
+++ packages/supercompat/src/adapters/client/anthropicClientAdapter/completions/serializeTools.ts
@@ -3,9 +3,9 @@ import type OpenAI from 'openai'
 export const serializeTools = ({
   tools,
 }: {
-  tools: OpenAI.Beta.AssistantTool[]
+  tools: OpenAI.Beta.AssistantTool[] | undefined
 }) => (
-  tools.map((tool: OpenAI.Beta.AssistantTool) => {
+  (tools ?? []).map((tool: OpenAI.Beta.AssistantTool) => {
     if (tool.type === 'function') {
       return {
         name: tool.function.name,
