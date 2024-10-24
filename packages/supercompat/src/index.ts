ca2af844f0eeea7daed31106eb2275788a6ba244
Domas <domas.bitvinskas@me.com>
2024-10-25 00:04:17 +0300

    supercompat@2.7.0: add azureOpenaiClientAdapter with support for compiled environments

--- packages/supercompat/src/index.ts
+++ packages/supercompat/src/index.ts
@@ -1,6 +1,7 @@
 export { supercompat } from './supercompat'
 export { groqClientAdapter } from './adapters/client/groqClientAdapter'
 export { openaiClientAdapter } from './adapters/client/openaiClientAdapter'
+export { azureOpenaiClientAdapter } from './adapters/client/azureOpenaiClientAdapter'
 export { mistralClientAdapter } from './adapters/client/mistralClientAdapter'
 export { perplexityClientAdapter } from './adapters/client/perplexityClientAdapter'
 export { anthropicClientAdapter } from './adapters/client/anthropicClientAdapter'
