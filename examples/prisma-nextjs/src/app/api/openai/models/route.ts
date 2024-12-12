5e7035683eb543dededc9b75e05a05d0c83ff4bf
Domas <domas.bitvinskas@me.com>
2024-12-12 00:31:17 -0800

    Add azure and openai models examples

---
+++ examples/prisma-nextjs/src/app/api/openai/models/route.ts
@@ -0,0 +1,33 @@
+import OpenAI from 'openai'
+import { NextResponse } from 'next/server'
+import {
+  supercompat,
+  openaiClientAdapter,
+  prismaStorageAdapter,
+  completionsRunAdapter,
+} from 'supercompat'
+import { prisma } from '@/lib/prisma'
+
+export const GET = async () => {
+  const client = supercompat({
+    client: openaiClientAdapter({
+      openai: new OpenAI(),
+    }),
+    storage: prismaStorageAdapter({
+      prisma,
+    }),
+    runAdapter: completionsRunAdapter(),
+  })
+
+  const response = await client.models.list()
+
+  const models = []
+
+  for await (const model of response) {
+    models.push(model)
+  }
+
+  return NextResponse.json({
+    models,
+  })
+}
