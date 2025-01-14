4d03c51d9daacf90860f3eb1260d1bf3ea3e1ee2
Domas <domas.bitvinskas@me.com>
2025-01-14 03:34:25 +0200

    supercompat@2.19.1: handle nested empty messages better

--- packages/supercompat/src/lib/messages/nonEmptyMessages.ts
+++ packages/supercompat/src/lib/messages/nonEmptyMessages.ts
@@ -1,17 +1,52 @@
 import type OpenAI from 'openai'
-import { isEmpty } from 'radash'
+import { isEmpty, isArray } from 'radash'
+
+const nonEmptyContent = ({
+  message,
+}: {
+  message: OpenAI.Chat.ChatCompletionMessageParam
+}): OpenAI.Chat.ChatCompletionMessageParam['content'] => {
+  if (isEmpty({ value: message.content })) {
+    return '-'
+  }
+
+  if (isArray(message.content)) {
+    return message.content.map((content) => {
+      if (content.type === 'text') {
+        if (isEmpty(content.text)) {
+          return {
+            type: 'text',
+            text: '-',
+          }
+        } else {
+          return content
+        }
+      } else {
+        return content
+      }
+    }) as OpenAI.Chat.ChatCompletionMessageParam['content']
+  }
+
+  return message.content as OpenAI.Chat.ChatCompletionMessageParam["content"]
+}
+
+type ExtendedRole = OpenAI.Chat.ChatCompletionMessageParam['role'] | 'developer'
+
+type ExtendedMessageParam = Omit<OpenAI.Chat.ChatCompletionMessageParam, 'role'> & {
+  role: ExtendedRole
+}
 
 export const nonEmptyMessages = ({
   messages,
 }: {
   messages: OpenAI.Chat.ChatCompletionMessageParam[]
 }) => {
-  const result = [] as OpenAI.Chat.ChatCompletionMessageParam[]
+  const result = [] as ExtendedMessageParam[]
 
   messages.forEach((message: OpenAI.Chat.ChatCompletionMessageParam) => (
     result.push({
       ...message,
-      content: isEmpty(message.content) ? '-' : message.content as string,
+      content: nonEmptyContent({ message }) as OpenAI.Chat.ChatCompletionMessageParam['content'],
     })
   ))
 
