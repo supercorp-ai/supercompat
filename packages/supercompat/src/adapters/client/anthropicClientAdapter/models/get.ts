7f6ebffe4fc947cc377d9a1d6b6c6c77507ccfda
Domas <domas.bitvinskas@me.com>
2025-02-24 16:43:00 -0300

    supercompat@2.22.0: add sonnet 3.7 support

--- packages/supercompat/src/adapters/client/anthropicClientAdapter/models/get.ts
+++ packages/supercompat/src/adapters/client/anthropicClientAdapter/models/get.ts
@@ -1,6 +1,9 @@
 import type Anthropic from '@anthropic-ai/sdk'
 
 const models = [
+  'claude-3-7-sonnet-latest',
+  'claude-3-7-sonnet-20250219',
+  'claude-3-5-haiku-latest',
   'claude-3-5-sonnet-20241022',
   'claude-3-5-sonnet-20240620',
   'claude-3-opus-20240229',
