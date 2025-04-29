756caae310be2082283ae9f02487614f13cec4ab
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-04-30 00:29:03 +0300

    v2.27.0: Add latest Gemini models (gemini-2.5-flash-preview-04-17)

--- packages/supercompat/src/adapters/client/googleClientAdapter/models/get.ts
+++ packages/supercompat/src/adapters/client/googleClientAdapter/models/get.ts
@@ -1,19 +1,13 @@
 import type OpenAI from 'openai'
 
 const models = [
+  'gemini-2.5-flash-preview-04-17',
   'gemini-2.5-pro-preview-03-25',
-  'gemini-2.5-pro-exp-03-25',
-  'gemini-2.0-flash-exp',
   'gemini-2.0-flash',
-  'gemini-2.0-flash-lite-preview-02-05',
+  'gemini-2.0-flash-lite',
   'gemini-1.5-flash',
   'gemini-1.5-flash-8b',
   'gemini-1.5-pro',
-  'gemma-3-27b-it',
-  'gemma-3-12b-it',
-  'gemma-3-4b-it',
-  'text-embedding-004',
-  'aqa',
 ]
 
 export const get = ({
