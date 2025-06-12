9ab02b861f3908723914ddc39e79bf28f208cbc0
Domas Bitvinskas <domas.bitvinskas@me.com>
2025-06-12 18:47:28 +0300

    supercompat@2.31.0: load models from Anthropic’s endpoint

--- packages/supercompat/src/adapters/client/anthropicClientAdapter/models/get.ts
+++ packages/supercompat/src/adapters/client/anthropicClientAdapter/models/get.ts
@@ -1,27 +1,17 @@
 import type Anthropic from '@anthropic-ai/sdk'
 
-const models = [
-  'claude-3-7-sonnet-latest',
-  'claude-3-7-sonnet-20250219',
-  'claude-3-5-haiku-latest',
-  'claude-3-5-sonnet-20241022',
-  'claude-3-5-sonnet-20240620',
-  'claude-3-opus-20240229',
-  'claude-3-sonnet-20240229',
-  'claude-3-5-haiku-20241022',
-  'claude-3-haiku-20240307',
-]
-
 export const get = ({
   anthropic,
 }: {
   anthropic: Anthropic
 }) => async (_url: string, _options: any) => {
   try {
+    const response = await anthropic.models.list()
+
     return new Response(JSON.stringify({
       type: 'list',
-      data: models.map((model) => ({
-        id: model,
+      data: response.data.map((model) => ({
+        id: model.id,
         object: 'model',
       })),
     }), {
